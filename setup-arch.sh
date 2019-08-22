f4a0c2d237fa525a4a2c3892cbcf846d60db4316
Ahmed Bilal Khalid <ahmedbilal96@gmail.com>
2019-08-22 22:51:34 +0500

    keychain added

--- setup-arch.sh
+++ setup-arch.sh
@@ -1,4 +1,4 @@
 sudo pacman -Syuu
-sudo pacman -S krdc trizen vlc libreoffice-fresh pamac-cli
-
+sudo pacman -S krdc trizen vlc libreoffice-fresh pamac-cli keychain
+echo "eval `keychain --eval --agents ssh id_rsa`" >> ~/.bashrc
 pamac build mattermost-desktop-bin --no-confirm
